d40295dbe8d0e4980d60dd86d2bd396ce92166bc
derick1129 <derickjohn1129@gmail.com>
2026-05-08 16:30:15 +0530

    completed dashboard page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import api from "../lib/axios";
+import Appbar from "../components/dashboard/Appbar";
+import Balance from "../components/dashboard/Balance";
+import Users from "../components/dashboard/Users";
+
 export default function Dashboard() {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    async function fetchBalance() {
+      try {
+        const response = await api.get("/account/balance");
+
+        setBalance(response.data.balance);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchBalance();
+  }, []);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="min-h-screen bg-slate-100">
+      <Appbar name="Derick" />
+      <Balance value={balance} />
+      <Users />
+    </div>
+  );
 }
